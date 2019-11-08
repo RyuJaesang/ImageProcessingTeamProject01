70be09f56e5a9d436b56fd1221a5adcc63112551
RyuJaesang <rjs9611@naver.com>
2019-11-08 16:25:04 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+
+
+int main()
+{
+	Mat img_color = imread("C:\\Users\\RyuJaesang\\Desktop\\IPTeamProject01\\triangle3.png", IMREAD_COLOR);
+
+
+	int height = img_color.rows;
+	int width = img_color.cols;
+
+
+	Mat img_grayscale(height, width, CV_8UC1);
+
+	//grayscaling
+	for (int y = 0; y < height; y++) {
+
+		uchar* pointer_input = img_color.ptr<uchar>(y);
+		uchar* pointer_ouput = img_grayscale.ptr<uchar>(y);
+		for (int x = 0; x < width; x++) {
+
+			// 컬러영상의 경우 픽셀값 읽어오기
+			uchar b = pointer_input[x * 3 + 0];
+			uchar g = pointer_input[x * 3 + 1];
+			uchar r = pointer_input[x * 3 + 2];
+
+			//그레이스케일의 경우 픽셀값 저장하기
+			pointer_ouput[x] = (r + g + b) / 3.0;
+		}
+	}
+
+	Mat img_binaryscale(height, width, CV_8UC1);
+
+	int threshold = 128;
+	int intensity;
+
+	for (int y = 0; y < height; y++) {
+		for (int x = 0; x < width; x++) {
+			intensity = img_grayscale.at<uchar>(y, x);
+
+			if (intensity > threshold) {
+				img_binaryscale.at<uchar>(y, x) = uchar(255);
+			}
+			else {
+				img_binaryscale.at<uchar>(y, x) = uchar(0);
+			}
+		}
+	}
+
+
+
+	imshow("color", img_color);
+	imshow("grayscale", img_grayscale);
+	imshow("binaryscale", img_binaryscale);
+
+	waitKey(0);
+
+
+	return 0;
+}
+
+
+
+
+
+
