e647fb001737bc7752ea88c197ae74ab74c11116
RyuJaesang <rjs9611@naver.com>
2019-11-08 16:39:26 +0900

    functionalize first commit

--- main.cpp
+++ main.cpp
@@ -5,22 +5,13 @@ using namespace cv;
 using namespace std;
 
 
-
-int main()
-{
-	Mat img_color = imread("C:\\Users\\RyuJaesang\\Desktop\\IPTeamProject01\\triangle3.png", IMREAD_COLOR);
-
-
-	int height = img_color.rows;
-	int width = img_color.cols;
-
-
+Mat grayscaling(Mat input, int height, int width) {
 	Mat img_grayscale(height, width, CV_8UC1);
 
 	//grayscaling
 	for (int y = 0; y < height; y++) {
 
-		uchar* pointer_input = img_color.ptr<uchar>(y);
+		uchar* pointer_input = input.ptr<uchar>(y);
 		uchar* pointer_ouput = img_grayscale.ptr<uchar>(y);
 		for (int x = 0; x < width; x++) {
 
@@ -33,15 +24,17 @@ int main()
 			pointer_ouput[x] = (r + g + b) / 3.0;
 		}
 	}
+	return img_grayscale;
+}
 
+Mat binarycaling(Mat input, int height, int width) {
 	Mat img_binaryscale(height, width, CV_8UC1);
-
 	int threshold = 128;
 	int intensity;
 
 	for (int y = 0; y < height; y++) {
 		for (int x = 0; x < width; x++) {
-			intensity = img_grayscale.at<uchar>(y, x);
+			intensity = input.at<uchar>(y, x);
 
 			if (intensity > threshold) {
 				img_binaryscale.at<uchar>(y, x) = uchar(255);
@@ -51,6 +44,33 @@ int main()
 			}
 		}
 	}
+
+	return img_binaryscale;
+}
+
+
+
+int main()
+{
+	Mat img_color = imread("C:\\Users\\RyuJaesang\\Desktop\\IPTeamProject01\\triangle3.png", IMREAD_COLOR);
+	int height = img_color.rows;
+	int width = img_color.cols;
+
+
+	Mat img_grayscale = grayscaling(img_color, height, width);
+	Mat img_binaryscale = binarycaling(img_grayscale, height, width);
+	
+
+	/*
+	for (int y = 0; y < height; y++) {
+		for (int x = 0; x < width; x++) {
+			intensity = img_grayscale.at<uchar>(y, x);
+
+
+
+		}
+	}
+	*/
 
 
 
