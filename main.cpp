f5aa97929755b4dd69e2c62fe7a37377282f74b2
Yeongeun Kang <39408863+yeonun@users.noreply.github.com>
2019-11-08 20:52:39 +0900

    yeongeun finished!
    detect point struct, functionalize (findsidepoints, distinguish)
    + input image file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
+
 using namespace cv;
 using namespace std;
 
+typedef struct {
+	int row;
+	int col;
+} Dot;
+
+typedef struct {
+	Dot upside;
+	Dot leftside;
+} SideDetect;
 
 Mat grayscaling(Mat input, int height, int width) {
 	Mat img_grayscale(height, width, CV_8UC1);
@@ -48,108 +58,161 @@ Mat binarycaling(Mat input, int height, int width) {
 	return img_binaryscale;
 }
 
+SideDetect findsidepoints(Mat img_binaryscale, int height, int width) {
 
-
-
-
-int main()
-{
-	Mat img_color = imread("C:\\Users\\RyuJaesang\\Desktop\\IPTeamProject01\\triangle3.png", IMREAD_COLOR);
-	int height = img_color.rows;
-	int width = img_color.cols;
-
-
-	Mat img_grayscale = grayscaling(img_color, height, width);
-	Mat img_binaryscale = binarycaling(img_grayscale, height, width);
-	printf("IMP is done\n");
-	printf("height is %d", height);
-	printf("width is %d", width);
-
-	//upsideXY
+	SideDetect tempSidePoints;
+		//upsideXY
 	bool upsideDetect = false;
 	int upsideX = 0;
 	int upsideY = 0;
+	int i, j;
 	int intensity;
-	for (upsideY; upsideY < height; upsideY++) {
-		for (upsideX; upsideX < width; upsideX++) {
-			intensity = img_binaryscale.at<uchar>(upsideY, upsideX);
+
+	for (i = 0; i < height; i++) {
+		for (j = 0; j < width; j++) {
+			intensity = img_binaryscale.at<uchar>(i, j);
 			if (intensity == 0) {
+				upsideY = i;
+				upsideX = j;
 				upsideDetect = true;
+			//	printf("intensity(%d, %d) is %d\n", upsideX, upsideY, intensity);
 			}
-			printf(upsideDetect ? "true" : "false");
-			printf("intensity(%d, %d) is %d\n", upsideX, upsideY, intensity);
 			if (upsideDetect) {
 				break;
 			}
 		}
 		if (upsideDetect) {
-			break;//브레이크 계속 걸림
-
+			break;
 		}
+
 	}
 
 	//leftsideXY
 	bool leftsideDetect = false;
 	int leftsideX = 0;
 	int leftsideY = 0;
-	for (leftsideY; leftsideY < height; leftsideY++) {
-		for (leftsideX; leftsideX < width; leftsideX++) {
-			intensity = img_binaryscale.at<uchar>(leftsideY, leftsideX);
+	int k, l;
+
+	for (k = 0; k < width; k++) {
+		for (l = 0; l < height; l++) {
+			intensity = img_binaryscale.at<uchar>(l, k);
 			if (intensity == 0) {
+				leftsideY = l;
+				leftsideX = k;
 				leftsideDetect = true;
+			//	printf("intensity(%d, %d) is %d\n", leftsideX, leftsideY, intensity);
 			}
-			if (leftsideDetect) break;
+			if (leftsideDetect) {
+				break;
+			}
 		}
-		if (leftsideDetect) break;
+		if (leftsideDetect) {
+			break;
+		}
+
 	}
-	printf("side detect is done\n");
-	
-	//DetectShpae
-	int medianX = (upsideX + leftsideX) / 2;
-	int medianY = (upsideY + leftsideY) / 2;
-	printf("upsideX is %d\n", upsideX);
-	printf("upsideY is %d\n", upsideY);
+	tempSidePoints.upside.row = upsideY;
+	tempSidePoints.upside.col = upsideX;
+	tempSidePoints.leftside.row = leftsideY;
+	tempSidePoints.leftside.col = leftsideX;
 
-	printf("leftsideX is %d\n", leftsideX);
-	printf("leftsideY is %d\n", leftsideY);
-	
+	return tempSidePoints;
+}
 
-	printf("medianX is %d\n", medianX);
-	printf("width is %d\n", width);
-	printf("height is %d\n", height);
+void distinguish (Mat img_binaryscale, int height, SideDetect sidepoints) {
+	SideDetect mid;
+	Dot real_mid;
+
+	mid.upside.col = (sidepoints.upside.col + sidepoints.leftside.col) / 2;
+	//printf("up : %d \n", sidepoints.upside.col);	
+	//printf("left : %d \n", sidepoints.leftside.col);
+	//printf("mid : %d \n", mid.upside.col);
+	mid.upside.row = (sidepoints.upside.row + sidepoints.leftside.row) / 2;
 
 	int testY = 0;
 	bool shapeDetect = false;
 	for (testY; testY < height; testY++) {
-		int intensity = img_binaryscale.at<uchar>(testY, medianX);
+		int intensity = img_binaryscale.at<uchar>(testY, mid.upside.col);
 		if (intensity == 0) {
 			shapeDetect = true;
+		//	printf("testY is %d\n", testY);
+		//	printf("%d\n", mid.upside.row);
 		}
 		if (shapeDetect) break;
 	}
 
-	if (testY == medianY) {
-		printf("Triangle is detected");
+	if (abs(testY - mid.upside.row) < 4) {
+		printf("Triangle \n");
 	}
 	else {
-		printf("Circle is detected");
+		printf("Circle \n");
 	}
+}
+
+int main()
+{
+
+	String srcImageName_a = "C:\\input\\a.jpg";
+	String srcImageName_b = "C:\\input\\b.jpg";
+	String srcImageName_c = "C:\\input\\c.jpg";
+	String srcImageName_d = "C:\\input\\d.jpg";
+
+	Mat img_color_a = imread(srcImageName_a, IMREAD_COLOR);
+	int height_a = img_color_a.rows;
+	int width_a = img_color_a.cols; 
+	Mat img_color_b = imread(srcImageName_b, IMREAD_COLOR);
+	int height_b = img_color_b.rows;
+	int width_b = img_color_b.cols;
+	Mat img_color_c = imread(srcImageName_c, IMREAD_COLOR);
+	int height_c = img_color_c.rows;
+	int width_c = img_color_c.cols;
+	Mat img_color_d = imread(srcImageName_d, IMREAD_COLOR);
+	int height_d = img_color_d.rows;
+	int width_d = img_color_d.cols;
+
+	printf("Enter image :\n");
+	cout << srcImageName_a << endl;
+	cout << srcImageName_b << endl;
+	cout << srcImageName_c << endl;
+	cout << srcImageName_d << endl;
+	printf("\n");
+
+	Mat img_grayscale_a = grayscaling(img_color_a, height_a, width_a);
+	Mat img_grayscale_b = grayscaling(img_color_b, height_b, width_b);
+	Mat img_grayscale_c = grayscaling(img_color_c, height_c, width_c);
+	Mat img_grayscale_d = grayscaling(img_color_d, height_d, width_d);
+
+	Mat img_binaryscale_a = binarycaling(img_grayscale_a, height_a, width_a);
+	Mat img_binaryscale_b = binarycaling(img_grayscale_b, height_b, width_b);
+	Mat img_binaryscale_c = binarycaling(img_grayscale_c, height_c, width_c);
+	Mat img_binaryscale_d = binarycaling(img_grayscale_d, height_d, width_d);
+
+	//printf("IMP is done\n");
+	//printf("height is %d \n", height_a);
+	//printf("width is %d \n", width_a);
+
+	SideDetect sideDetect_a = findsidepoints(img_binaryscale_a,height_a,width_a);
+	SideDetect sideDetect_b = findsidepoints(img_binaryscale_b, height_b, width_b);
+	SideDetect sideDetect_c = findsidepoints(img_binaryscale_c, height_c, width_c);
+	SideDetect sideDetect_d = findsidepoints(img_binaryscale_d, height_d, width_d);
 
 
+	printf("Result :\n");
+	distinguish(img_binaryscale_a, height_a, sideDetect_a);
+	distinguish(img_binaryscale_b, height_b, sideDetect_b);
+	distinguish(img_binaryscale_c, height_c, sideDetect_c);
+	distinguish(img_binaryscale_d, height_d, sideDetect_d);
+	printf("\n");
 
 
-	imshow("color", img_color);
-	imshow("grayscale", img_grayscale);
-	imshow("binaryscale", img_binaryscale);
+	//imshow("color", img_color_a);
+	//imshow("grayscale", img_grayscale_a);
+	//imshow("binaryscale", img_binaryscale_a);
 
 	waitKey(0);
 
 
 	return 0;
 }
-
-
-
-
 
 
